b2b9a3c3f6cdf8dbd873b33fd577402f0451f08f
bagahizriev <bagahizriev@yandex.ru>
2024-11-22 20:09:46 +0400

    ваши изменения

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd /home/botuser/ваш-репозиторий
+cd /home/botuser/reminderhero
+git config --global --add safe.directory /home/botuser/reminderhero
 git pull
-source venv/bin/activate
+source .venv/bin/activate
 pip install -r requirements.txt
 sudo supervisorctl restart tgbot 
