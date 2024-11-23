6235c337b887015b6cd18ae2440c6f7963c54b63
bagahizriev <bagahizriev@yandex.ru>
2024-11-23 09:57:52 +0400

    Изменение скрипта update для отслеживания только main ветки

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /home/botuser/reminderhero
 git config --global --add safe.directory /home/botuser/reminderhero
-git pull
+git fetch origin main      # Получаем изменения только из main ветки
+git reset --hard origin/main  # Принудительно синхронизируем с main
 source .venv/bin/activate
 pip install -r requirements.txt
 sudo supervisorctl restart tgbot 
